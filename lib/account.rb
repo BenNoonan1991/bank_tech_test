c708e481c2a6117439b6918fdfd0394c9b9d88e5
BenNoonan1991 <ben.noonan2@gmail.com>
2017-06-12 12:36:39 +0100

    Adds credit method and test

--- lib/account.rb
+++ lib/account.rb
@@ -1,9 +1,14 @@
 class Account
 
   attr_reader :balance
+  attr_writer :balance
 
   def initialize
     @balance = 0
+  end
+
+  def credit(amount)
+    self.balance += amount
   end
 
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -8,4 +8,11 @@ describe Account do
       expect(account.balance).to eq 0
     end
   end
+
+  describe "#credit" do
+    it "deposits money into the account" do
+      account.credit(100)
+      expect(account.balance).to eq 100
+    end
+  end
 end
