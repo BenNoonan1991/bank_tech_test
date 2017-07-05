c73905bfb636562603f0309c6ddda43c7e8b7b10
BenNoonan1991 <ben.noonan2@gmail.com>
2017-07-05 19:30:36 +0100

    Remove commented out lines

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -2,7 +2,6 @@ require 'account'
 
 describe Account do
   subject(:account) { described_class.new }
-  #let(:date) { Time.now.strftime "%d/%m/%Y" }
 
   describe "#initialize" do
     it "starts with a balance of 0" do
